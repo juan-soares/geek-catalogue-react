b920c63fc60f247cd6f66c1945c043490a2c31fa
Juan Carlos <juan.css013@gmail.com>
2023-01-27 12:38:04 +0000

    refactor: Refactored behave of admin resources.

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -2,30 +2,42 @@ import { useState } from "react";
 import ResourceFeatures from "../../components/ResourceFeatures";
 
 export default function AdminPage() {
+  const [resourceSelected, setResourceSelected] = useState({
+    title: "",
+    url: "",
+  });
   const resources = [
     { title: "Categorias", url: "category" },
-    { title: "Subcategorias", url: "subcategory" },
+    { title: "Subcategorias", url: "category/subcategory" },
+    { title: "Idiomas", url: "category/language" },
+    { title: "Qualidades", url: "category/resolution" },
+    { title: "Edições", url: "category/edition" },
+    { title: "Gêneros", url: "category/genre" },
+    { title: "Modalidades", url: "category/modality" },
+    { title: "Movimentações", url: "category/movement" },
+    { title: "Temáticas", url: "theme" },
+    { title: "Franquias", url: "franchise" },
   ];
-  const [showResourceFeatures, setShowResourceFeatures] = useState(false);
-  const [selectedResource, setSelectedResource] = useState({});
 
   return (
     <div>
       ADMIN PAGE
       {resources.map((resource) => {
         return (
-          <button
-            key={resource.url}
-            onClick={() => {
-              setShowResourceFeatures(true);
-              setSelectedResource(resource);
-            }}
-          >
-            {resource.title}
-          </button>
+          <nav key={resource.url}>
+            <button
+              onClick={() => {
+                setResourceSelected(resource);
+              }}
+            >
+              {resource.title}
+            </button>
+          </nav>
         );
       })}
-      {showResourceFeatures && <ResourceFeatures resource={selectedResource} />}
+      {resourceSelected.url !== "" && (
+        <ResourceFeatures resourceSelected={resourceSelected} />
+      )}
     </div>
   );
 }
